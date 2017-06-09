064ace6bd30cb3a0baee06096c91146e62e26415
Gav <gav.aiken@gmail.com>
2017-06-09 13:06:45 -0700

    Initial Commit

---
+++ FactFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FactFeed
+//
+//  Created by Gavin Aiken on 6/9/17.
+//  Copyright © 2017 Gavin Aiken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
