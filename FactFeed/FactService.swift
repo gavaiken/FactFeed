4e4c27bb552ca8f3da480a8ce415576d039174c9
Gav <gav.aiken@gmail.com>
2017-06-10 15:30:56 -0700

    Cleanup after a Joe Laws code review

--- FactFeed/FactService.swift
+++ FactFeed/FactService.swift
@@ -2,16 +2,16 @@ import UIKit
 import reddift
 
 struct FFSubredditURLPath: SubredditURLPath {
-  let path: String;
+  let path: String
 }
 
 struct Fact {
-  let title: String;
+  let title: String
 }
 
-class FactService: NSObject {
-  let subreddit = FFSubredditURLPath(path: "r/todayilearned")
-  var paginator: Paginator? = Paginator()
+class FactService {
+  fileprivate let subreddit = FFSubredditURLPath(path: "r/todayilearned")
+  fileprivate var paginator: Paginator? = Paginator()
 
   func fetchNextFacts(completion: @escaping (Result<[Fact]>) -> Void) {
     guard let paginator = paginator else {
@@ -20,40 +20,45 @@ class FactService: NSObject {
     }
 
     do {
-      try Session().getList(paginator, subreddit:subreddit, sort: .top, timeFilterWithin: .all) { (result) -> Void in
-        switch result {
-        case .failure(let error):
-          print(error)
-        case .success:
-          if let value = result.value {
-            self.paginator = FactService.getNextPaginator(paginator: value.paginator)
-            let links = value.children.flatMap { $0 as? Link }
-            let facts = links.map({ (link) -> Fact in
-              Fact(title: link.title)
-            })
-            completion(Result(value: facts))
-          }
-        }
+      try Session().getList(paginator, subreddit:subreddit, sort: .top, timeFilterWithin: .all) { [weak self] (result) -> Void in
+        self?.process(result: result, completion: completion)
       }
     } catch {
       completion(Result<[Fact]>(error: error as NSError))
     }
   }
   
-  class func getNextPaginator(paginator: Paginator?) -> Paginator? {
-    if let prevPaginator = paginator {
-      let dict = prevPaginator.parameterDictionary
-      if let before = dict["before"] {
-        // Set the 'before' parameter
-        let nextPaginator = Paginator(after: "", before: before, modhash: "")
-        return nextPaginator
+  private func process(result: Result<Listing>, completion: @escaping (Result<[Fact]>) -> Void) {
+    switch result {
+    case .failure(let error):
+      completion(Result<[Fact]>(error: error as NSError))
+    case .success:
+      if let value = result.value {
+        self.paginator = FactService.paginator(after: value.paginator)
+        let links = value.children.flatMap { $0 as? Link }
+        let facts = links.map({ (link) -> Fact in
+          Fact(title: link.title)
+        })
+        completion(Result(value: facts))
       } else {
-        // The current page did not come 'before' another page we can fetch.
-        return nil;
+        completion(Result(error: nil))
       }
-    } else {
+    }
+  }
+  
+  private class func paginator(after paginator: Paginator?) -> Paginator? {
+    guard let paginator = paginator else {
       // First fetch.
       return Paginator()
     }
+
+    guard let before = paginator.parameterDictionary["before"] else {
+      // The current page did not come 'before' another page we can fetch.
+      return nil
+    }
+
+    // Set the 'before' parameter to query the 'next' results.
+    let nextPaginator = Paginator(after: "", before: before, modhash: "")
+    return nextPaginator
   }
 }
